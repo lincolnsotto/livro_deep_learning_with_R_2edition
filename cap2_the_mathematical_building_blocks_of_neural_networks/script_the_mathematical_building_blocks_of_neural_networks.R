ff2d27614fa6348d840d16c7a3801d08476251c5
Lincoln Sotto <lincoln.sotto@hotmail.com>
2024-06-11 17:00:54 -0300

    20240611 upload alteracoes no cap2

--- cap2_the_mathematical_building_blocks_of_neural_networks/script_the_mathematical_building_blocks_of_neural_networks.R
+++ cap2_the_mathematical_building_blocks_of_neural_networks/script_the_mathematical_building_blocks_of_neural_networks.R
@@ -1,13 +1,31 @@
+########################################################################
+### Capítulo 2 - The Mathematical Building Blocks of Neural Networks ###
+########################################################################
 
+#Instalando e carregando as bibliotecas necessárias
 install.packages("keras") 
-
-library(reticulate)
-virtualenv_create("r-reticulate", python = install_python())
+install.packages("tensorflow")
+install.packages("reticulate")
 
 library(keras)
+library(tensorflow)
+library(reticulate)
+
+#Criando o ambiente virtual para instalação do python e keras
+
+virtualenv_create("r-reticulate", python = install_python())
 install_keras(envname = "r-reticulate", version = "2.16.1")
 
-mnist <- dataset_mnist()
+#Carregando a base MNIST localmente
 
-setwd("/Users/lincoln/Documents/GitHub/livro_deep_learning_with_R_2edition/cap2_the_mathematical_building_blocks_of_neural_networks")
+#Não consegui acessar o link diretamente devido ao protocolo de certificação SSL
+#Por este motivo baiexei o arquivo mnist.npz e salvando na pasta do livro e fiz a abertura diretamente
+caminho <- setwd("/Users/lincoln/Documents/GitHub/livro_deep_learning_with_R_2edition/cap2_the_mathematical_building_blocks_of_neural_networks")
+mnist <- dataset_mnist(path = paste(caminho,"/mnist.npz", sep=""))
+
+#Analisando a base MNIST
+mnist
+
+digit <- mnist$train$x[1,,]
+plot(as.raster(digit, max = 255))
 
