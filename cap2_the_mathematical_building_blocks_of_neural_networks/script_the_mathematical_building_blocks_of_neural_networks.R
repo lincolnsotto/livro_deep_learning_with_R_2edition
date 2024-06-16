3ca61d25a74b736ec14dec575cfb6da82fce140e
Lincoln Sotto <lincoln.sotto@hotmail.com>
2024-06-16 12:57:38 -0300

    20240616

--- cap2_the_mathematical_building_blocks_of_neural_networks/script_the_mathematical_building_blocks_of_neural_networks.R
+++ cap2_the_mathematical_building_blocks_of_neural_networks/script_the_mathematical_building_blocks_of_neural_networks.R
@@ -40,3 +40,35 @@ use_condaenv("r-reticulate", required = TRUE)
 
 
 use_condaenv("tf-m1", required = TRUE)
+
+mnist <- dataset_mnist()
+
+train_images <- mnist$train$x
+train_labels <- mnist$train$y
+test_images <- mnist$test$x
+test_labels <- mnist$test$y
+
+model <- keras_model_sequential(list(
+  layer_dense(units = 512, activation = "relu"),
+  layer_dense(units = 10, activation = "softmax")
+))
+
+
+train_images <- array_reshape(train_images, c(60000, 28 * 28))
+train_images <- train_images / 255
+test_images <- array_reshape(test_images, c(10000, 28 * 28))
+test_images <- test_images / 255
+
+keras::compile(model,
+        optimizer = "rmsprop",
+        loss = "sparse_categorical_crossentropy",
+        metrics = "accuracy")
+
+train_images <- array_reshape(train_images, c(60000, 28 * 28))
+train_images <- train_images / 255
+test_images <- array_reshape(test_images, c(10000, 28 * 28))
+test_images <- test_images / 255
+
+fit(model, train_images, train_labels, epochs = 5, batch_size = 128)
+
+
